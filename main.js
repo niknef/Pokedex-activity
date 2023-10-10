7965c04d0fe74b35c32aef24dbda35a6d5bfed39
Nicolas Firpo <nicolas.firpo@davinci.edu.ar>
2023-10-10 18:51:51 -0300

    add stats to modal

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
 // Seleccionamos el contenedor HTML con la clase "cardContainer"
-const cardContainer = document.querySelector(".row");
+
+const cardContainer = document.querySelector('.row');
+const modal = document.querySelector('#pokemonModal');
 
 // Función para obtener datos de un Pokémon específico mediante su ID
-function fetchPokemon(id){
+
+function fetchPokemon(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
         .then((res) => res.json())
         .then((data) => {
             createPokemon(data);
         });
 }
-
 // Función para obtener varios Pokémon a la vez
 function fetchPokemons() {
-    for (let i = 1; i <= 100; i++) { 
+    for (let i = 1; i <= 100; i++) {
         fetchPokemon(i);
-    }   
+    }
 }
 
 // Función para crear y mostrar un elemento visual para un Pokémon
-function createPokemon(pokemon){
-    // Creamos los elementos que van dentro de la tarjeta (card)
+function createPokemon(pokemon) {
     const col = document.createElement('div');
     col.classList.add('col');
 
     const card = document.createElement('div');
     card.classList.add('card', 'h-100');
-    
+
     const sprite = document.createElement('img');
     sprite.src = pokemon.sprites.front_default;
     sprite.classList.add('card-img-top');
@@ -35,17 +36,26 @@ function createPokemon(pokemon){
 
     const name = document.createElement('h5');
     name.classList.add('card-title', 'text-secondary');
-    name.textContent = pokemon.name + ` #${pokemon.id}`; 
+    name.textContent = pokemon.name + ` #${pokemon.id}`;
 
     const url = document.createElement('p');
     url.classList.add('card-text', 'text-secondary');
     url.textContent = pokemon.species.url;
 
     const button = document.createElement('button');
-    button.classList.add('btn', 'btn-primary'); 
+    button.classList.add('btn', 'btn-primary');
     button.setAttribute('data-bs-toggle', 'modal');
     button.setAttribute('data-bs-target', '#pokemonModal');
     button.textContent = 'Stats';
+
+    // Asocia los datos del Pokémon con el botón
+    button.setAttribute('data-pokemon', JSON.stringify(pokemon));
+
+    // Agrega un controlador de eventos al botón "Stats"
+    button.addEventListener('click', (event) => {
+        const pokemonData = JSON.parse(event.currentTarget.getAttribute('data-pokemon'));
+        renderModal(pokemonData);
+    });
 
     //agregamos los elementos que van dentro del body de nuestra tarjeta
     cardBody.appendChild(name);
@@ -56,15 +66,59 @@ function createPokemon(pokemon){
     card.appendChild(sprite);
     card.appendChild(cardBody);
 
-    //agregamos el contenido de la card al div col  
+    //agregamos el contenido de la card al div col 
     col.appendChild(card);
     // Agregamos la tarjeta al contenedor
     cardContainer.appendChild(col);
-};
-
-//modal
+}
 
 
+function progressBars(stats) {
+    // ... Crea y estructura barras de progreso para las estadísticas del Pokémon ...
+    const statsContainer = document.createElement("div");
+    statsContainer.classList.add("stats-container");
+  
+    for (let i = 0; i < 3; i++) {
+      const stat = stats[i];
+  
+      const statPercent = stat.base_stat / 2 + "%";
+      const statContainer = document.createElement("stat-container");
+      statContainer.classList.add("stat-container");
+  
+      const statName = document.createElement("p");
+      statName.textContent = stat.stat.name;
+  
+      const progress = document.createElement("div");
+      progress.classList.add("progress");
+  
+      const progressBar = document.createElement("div");
+      progressBar.classList.add("progress-bar");
+      progressBar.setAttribute("aria-valuenow", stat.base_stat);
+      progressBar.setAttribute("aria-valuemin", 0);
+      progressBar.setAttribute("aria-valuemax", 200);
+      progressBar.style.width = statPercent;
+  
+      progressBar.textContent = stat.base_stat;
+  
+      progress.appendChild(progressBar);
+      statContainer.appendChild(statName);
+      statContainer.appendChild(progress);
+  
+      statsContainer.appendChild(statContainer);
+    }
+  
+    return statsContainer;
+  };
+
+  
+  //modal
+  const renderModal = (pokemon) => {
+    const name = modal.querySelector('.modal-title');
+    name.innerHTML = pokemon.name;
+    const stats = modal.querySelector('.modal-body');
+    stats.innerHTML = "";
+    stats.appendChild(progressBars(pokemon.stats));
+}
 
 // Llamamos a la función fetchPokemons para comenzar a obtener datos de Pokémon
 fetchPokemons();
